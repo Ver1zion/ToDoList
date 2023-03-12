f13845ccb26482411eb6a1f7f84d2aa57d4d8d58
Igor <smmdirektolog@yandex.ru>
2023-03-12 18:40:04 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -356,7 +356,7 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const requestAnimationFrame = window.requestAnimationFrame;
 
-canvas.height = window.innerHeight;
+canvas.height = document.body.offsetHeight;
 canvas.width = window.innerWidth;
 
 class StarryBackground {
